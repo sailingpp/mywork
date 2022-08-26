cb5ffa9a25244aacb0521d14dfac1c6e04349b56
sailingpp <103156278+sailingpp@users.noreply.github.com>
2022-08-26 15:26:57 +0800

    Add files via upload

---
+++ WinFormTest/Form1.cs
@@ -0,0 +1,41 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.Windows;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Document doc = Autodesk.AutoCAD.ApplicationServices.Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Circle cirl = new Circle();
+            cirl.Center = new Point3d(100, 100, 0);
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                BlockTable bt = trans.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                BlockTableRecord btr = trans.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                btr.AppendEntity(cirl);
+                trans.Commit();
+            }
+            db.SaveAs(@"E:\sapmodel\demo\test.dwg", DwgVersion.Current);
+        }
+    }
+}
