3ba885722ffb243ebc1017549b21b517a42ae6e9
sailingpp <103156278+sailingpp@users.noreply.github.com>
2022-08-26 15:23:26 +0800

    Add files via upload

---
+++ MyWork/MathHelper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWork
+{
+    public class MathHelper
+    {
+        /// <summary>
+        /// 弧度转角度
+        /// </summary>
+        /// <param name="rad"></param>
+        /// <returns></returns>
+        public static double RadToDeg(double rad)
+        {
+            double Deg = rad * 180/Math.PI;
+            return Deg;
+        }
+        /// <summary>
+        /// 角度转弧度
+        /// </summary>
+        /// <param name="deg"></param>
+        /// <returns></returns>
+        public static double DegToRad(double deg)
+        {
+            double Rad = deg *  Math.PI/180;
+            return Rad;
+        }
+
+
+    }
+}
---
+++ MyWork/SqlHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MyWork
+{
+    public class SqlHelper
+    {
+        public static string sqlcon;
+        public static DataTable ExcuteDataTable(string sql, params SqlParameter[] param)
+        {
+            DataTable dt = new DataTable();
+            using (SqlConnection conn = new SqlConnection(sqlcon))
+            {
+                conn.Open();
+                using (SqlDataAdapter sda = new SqlDataAdapter(sql, conn))
+                {
+                    sda.SelectCommand.Parameters.AddRange(param);
+                    sda.Fill(dt);
+                }
+            }
+            return dt;
+        }
+        public static int ExcuteNoquery(string sql, params SqlParameter[] param)
+        {
+            int n = -1;
+            using (SqlConnection conn = new SqlConnection(sqlcon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddRange(param);
+                    n = cmd.ExecuteNonQuery();
+                }
+            }
+            return n;
+        }
+        public static object ExcuteScalar(string sql, params SqlParameter[] param)
+        {
+            object obj = null;
+            using (SqlConnection conn = new SqlConnection(sqlcon))
+            {
+                conn.Open();
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddRange(param);
+                    obj = cmd.ExecuteScalar();
+                }
+            }
+            return obj;
+        }
+    }
+}
